3921a7e021608bbc2dde05ed2421946f8bd429ea
Filjok <filjofijothundiyil@gmail.com>
2022-12-13 14:44:37 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+
+import * as XLSX from 'xlsx'
 
 function App() {
+  
+  // on change states
+  const [excelFile, setExcelFile]=useState(null);
+  const [excelFileError, setExcelFileError]=useState(null);  
+ 
+  // submit
+  const [excelData, setExcelData]=useState(null);
+  // it will contain array of objects
+
+  // handle File
+  const fileType=[ 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+  const handleFile = (e)=>{
+    let selectedFile = e.target.files[0];
+    if(selectedFile){
+      // console.log(selectedFile.type);
+      if(selectedFile&&fileType.includes(selectedFile.type)){
+        let reader = new FileReader();
+        reader.readAsArrayBuffer(selectedFile);
+        reader.onload=(e)=>{
+          setExcelFileError(null);
+          setExcelFile(e.target.result);
+        } 
+      }
+      else{
+        setExcelFileError('invalid type');
+        setExcelFile(null);
+      }
+    }
+    
+  }
+
+  // submit function
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(excelFile!==null){
+      const workbook = XLSX.read(excelFile,{type:'buffer'});
+      const worksheetName = workbook.SheetNames[0];
+      const worksheet=workbook.Sheets[worksheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet);
+      // console.log(data);
+      setExcelData(data);
+    }
+    else{
+      setExcelData(null);
+    }
+    
+  }
+  useEffect(()=>{
+    console.log('this is excel data',excelData);
+  },[excelData])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      {/* upload file section */}
+      <div className='form'>
+        <form className='form-group' 
+        onSubmit={handleSubmit}>
+          <label><h5>Upload Excel file</h5></label>
+          <br></br>
+          <input type='file' className='form-control'
+          onChange={handleFile} required></input>                  
+          {excelFileError&&<div className='text-danger'
+          style={{marginTop:5+'px'}}>{excelFileError}</div>}
+          <button type='submit' className='btn btn-success'
+          style={{marginTop:5+'px'}}>Submit</button>
+        </form>
+      </div>
+
+      <br></br>
+      <hr></hr>
+
+      {/* view file section */}
+      
+
     </div>
   );
 }
